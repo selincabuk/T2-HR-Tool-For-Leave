597051effc56a257b1af0af49114446ceab80e2e
Selin Çabuk <selincbk13@gmail.com>
2023-07-13 11:06:07 +0300

    Update ExcuseLeaveSelect.tsx

--- frontend/src/UserPages/UserToolComponents/ExcuseLeaveSelect.tsx
+++ frontend/src/UserPages/UserToolComponents/ExcuseLeaveSelect.tsx
@@ -3,7 +3,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import "./UserPages.css";
+import "../UserPages.css";
 
 export default function SelectAutoWidth() {
     const [excuseLeave, setexcuseLeave] = React.useState('');
