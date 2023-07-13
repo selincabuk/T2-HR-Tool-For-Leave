839e08dab800b52e10948b5e5ea76bc4a6bc0123
CerenAkyr <cerenakyar@gmail.com>
2023-07-13 10:49:57 +0300

    :)

---
+++ frontend/src/UserPages/UserToolComponents/PreviousRequestsTable.tsx
@@ -0,0 +1,79 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import "./Profile.css";
+import { useState, useEffect } from 'react';
+
+function PreviousRequestsTable() {
+    type Requests = {
+        id: number,
+        startDate: string,
+        endDate: string,
+        status: string,
+    }
+    // state to fetch users:
+    const [requests, setRequests] = useState([] as Requests[]);
+
+    // state if fetching fails:
+    const [error, setError] = useState<boolean>(false);
+
+    // to show loading animation:
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true)
+            try {
+                const response = await fetch('http://localhost:3001/user/get-request');
+                if (!response.ok) {
+                    console.log("hataaaaaa")
+                }
+                const data = await response.json();
+                setRequests(data);
+                console.log(data);
+                setIsLoading(false)
+                setError(false)
+            } catch (error) {
+                setError(true)
+                setIsLoading(false)
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div className='profile__info__table'>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align='center'>Start Date</TableCell>
+                            <TableCell align="center">End Date</TableCell>
+                            <TableCell align="center">Status</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {requests.map((row) => (
+                            <TableRow
+                                key={row.id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row" align='center'>
+                                    {row.startDate}
+                                </TableCell>
+                                <TableCell align="center">{row.endDate}</TableCell>
+                                <TableCell align="center">{row.status}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    );
+}
+
+export default PreviousRequestsTable;
--- frontend/src/UserPages/ViewProfile.tsx
+++ frontend/src/UserPages/ViewProfile.tsx
@@ -1,13 +1,41 @@
 import NavigationBarUser from "./UserToolComponents/NavigationBarUser";
 import "./UserPages.css";
 import ProfileInfo from "./UserToolComponents/ProfileInfo";
+import React from 'react';
+import { Tabs } from 'antd';
+import PreviousRequestsTable from "./UserToolComponents/PreviousRequestsTable";
 
 
 function ViewProfile() {
-    return(
+    const onChange = (key: string) => {
+        console.log(key);
+    };
+
+    const items = [
+        {
+            key: "tab1",
+            label: "Profile Information",
+            children: <ProfileInfo />,
+        },
+        {
+            key: "tab2",
+            label: "Previous Requests",
+            children: <PreviousRequestsTable />,
+        },
+    ];
+
+    return (
         <div>
             <NavigationBarUser />
-            <ProfileInfo />
+            <div className="tab__div">
+                <Tabs
+                    size="large"
+                    onChange={onChange}
+                    type="card"
+                    items={items}
+
+                />
+            </div>
         </div>
     );
 }
