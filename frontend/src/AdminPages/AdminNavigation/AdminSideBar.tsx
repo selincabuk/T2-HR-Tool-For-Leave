a99b20b1acdbf504b8d73c25e75437135d5011e3
CerenAkyr <cerenakyar@gmail.com>
2023-07-19 16:59:53 +0300

    updates for sidebar

--- frontend/src/AdminPages/AdminNavigation/AdminSideBar.tsx
+++ frontend/src/AdminPages/AdminNavigation/AdminSideBar.tsx
@@ -15,6 +15,10 @@ import BadgeIcon from '@mui/icons-material/Badge';
 import RecentActorsOutlinedIcon from '@mui/icons-material/RecentActorsOutlined';
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 
+import Tooltip from '@mui/material/Tooltip';
+
+import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+
 
 interface MenuItem {
     title: string,
@@ -37,7 +41,7 @@ const sidebarItems: AdminSideBarMenu =
             icon: RecentActorsOutlinedIcon,
         },
         {
-            title: "Yeni Kullanıcı Kayıt",
+            title: "Yeni Kullanıcı",
             link: "/admin/register-user",
             icon: BadgeIcon,
         },
@@ -67,10 +71,10 @@ function AdminSideBar() {
     }, []);
 
     // to automatically close bar if window is resized:
-    useEffect( () => {
-        if (windowSize < 1000 )
+    useEffect(() => {
+        if (windowSize < 1000)
             setIsOpen(false)
-    }, [windowSize] );
+    }, [windowSize]);
 
     // button handler to check if the bar should be opened:
     const arrowButtonHandler = () => {
@@ -83,28 +87,42 @@ function AdminSideBar() {
     }
 
     return (
-        <Paper elevation={4} className="sidebar" style={isOpen ? { width: "15vw" } : { width: "70px" }}>
+        <Paper elevation={4} className="sidebar" style={ isOpen ? { width: "15vw" } : { width: "70px" }}>
             <List className="sidebar__first__list">
                 <Avatar className="sidebar__arrow"
-                    style={isOpen ? { transform: "rotate(180deg)", marginLeft: "14vw" } : { transform: "rotate(0deg)", marginLeft: "54px" }}
+                    style={isOpen ? { marginLeft: "14vw" } : { marginLeft: "54px" }}
                     onClick={arrowButtonHandler}>
-                    <ArrowForwardIosIcon />
+                    {isOpen && <MenuFoldOutlined />}
+                    {!isOpen && <MenuUnfoldOutlined />}
                 </Avatar>
             </List>
             <List className="sidebar__second__list">
-                {sidebarItems.map((item: MenuItem) =>
-                    <a href={item.link}>
-                        <ListItem className="sidebar__items">
-                            <ListItemAvatar>
-                                <Avatar className="sidebar__avatar">
-                                    <item.icon />
-                                </Avatar>
-                            </ListItemAvatar>
-                            {isOpen && <ListItemText primary={item.title} />}
-                        </ListItem>
-                        {isOpen ? <Divider variant="inset" component="li" className="sidebar__divider" />
-                            : <Divider variant="inset" component="li" className="sidebar__divider__closed" />}
-                    </a>
+                {sidebarItems.map((item: MenuItem) => 
+                        isOpen ?
+                            (<a href={item.link}>
+                                <ListItem className="sidebar__items">
+                                    <ListItemAvatar>
+                                        <Avatar className="sidebar__avatar">
+                                            <item.icon />
+                                        </Avatar>
+                                    </ListItemAvatar>
+                                    {isOpen && <ListItemText primary={item.title} />}
+                                </ListItem>
+                                <Divider variant="inset" component="li" className="sidebar__divider" />
+                            </a>)
+                            : (<Tooltip title={item.title} placement="right">
+                                <a href={item.link}>
+                                    <ListItem className="sidebar__items">
+                                        <ListItemAvatar>
+                                            <Avatar className="sidebar__avatar">
+                                                <item.icon />
+                                            </Avatar>
+                                        </ListItemAvatar>
+                                        {isOpen && <ListItemText primary={item.title} />}
+                                    </ListItem>
+                                    <Divider variant="inset" component="li" className="sidebar__divider__closed" />
+                                </a>
+                            </Tooltip>)
                 )}
             </List>
             <List className="sidebar__third__list">
