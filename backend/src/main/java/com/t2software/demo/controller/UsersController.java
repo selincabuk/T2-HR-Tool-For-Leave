bcd08246064c219dba83e661da0f71aab6e9fd1d
Selin Çabuk <selincbk13@gmail.com>
2023-07-24 15:52:31 +0300

    Update UsersController.java

--- backend/src/main/java/com/t2software/demo/controller/UsersController.java
+++ backend/src/main/java/com/t2software/demo/controller/UsersController.java
@@ -1,8 +1,11 @@
 package com.t2software.demo.controller;
 
 
+import com.t2software.demo.model.UsersModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class UsersController {
@@ -11,4 +14,12 @@ public class UsersController {
     public String getRegisterPage(){
         return "register_page";
     }
+
+   /* @GetMapping("/login")
+    public String getLoginPage(Model model){
+        return "login_page";
+    }
+
+    @PostMapping("/register")
+    public String register( )*/
 }
