524da22bf697cb1278cedd81dc92a09a5905f793
Selin Çabuk <selincbk13@gmail.com>
2023-07-26 11:38:32 +0300

    Update User.java

--- backend/src/main/java/com/t2software/demo/model/User.java
+++ backend/src/main/java/com/t2software/demo/model/User.java
@@ -82,13 +82,15 @@ import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.Collections;
 
 import java.util.Date;
 import java.util.UUID;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -96,6 +98,9 @@ public class User {
 
     @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
 
     @Column(name = "firstname", nullable = false)
     private String firstname;
@@ -125,8 +130,13 @@ public class User {
     private Boolean locked;
     private Boolean enabled;
 
-    
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority =
+                new SimpleGrantedAuthority(role.name());
+        return Collections.singletonList(authority);
+    }
 
     //  getter ve setter eklenecek
 
@@ -139,12 +149,21 @@ public class User {
         this.id = id;
     }
 
-    public String getEmail() {
+    public String getUsername() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getFirstname() {
